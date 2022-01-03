6e2eb6af28a99578236ca1c874a20026d47b4807
cspeloso <chrispeloso2@gmail.com>
2022-01-02 20:03:33 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Chris Peloso on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
